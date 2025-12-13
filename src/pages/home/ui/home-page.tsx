8a4850735089a70a0c3bfb31f1c6affd5d05691f
Jahongir <nusratovjahongir@gmail.com>
2025-12-13 22:28:22 +0500

    refactor: get user data

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -1,15 +1,24 @@
 import Wrapper from "@/shared/ui//custom/wrapper";
+import { useEffect, useState } from "react";
 import Menu from "./menu";
 
 const HomePage = () => {
+  const [user, setUser] = useState<TelegramUser | undefined>(undefined);
   const tg = window.Telegram?.WebApp;
   tg?.ready();
 
-  const user = tg?.initDataUnsafe?.user;
+  const tgUser = tg?.initDataUnsafe?.user;
+
+  useEffect(() => {
+    setUser(tgUser);
+    alert(JSON.stringify(tgUser));
+  }, [tgUser]);
+
   if (user) return <>user:{JSON.stringify(user)}</>;
 
   return (
     <Wrapper>
+      user
       <Menu />
     </Wrapper>
   );
