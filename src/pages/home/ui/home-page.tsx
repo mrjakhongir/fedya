e74b2e44ff28b32f6c455e9195a87fd53700a181
Jahongir <nusratovjahongir@gmail.com>
2025-12-13 22:05:02 +0500

    refactor: get user info

--- src/pages/home/ui/home-page.tsx
+++ src/pages/home/ui/home-page.tsx
@@ -2,8 +2,11 @@ import Wrapper from "@/shared/ui//custom/wrapper";
 import Menu from "./menu";
 
 const HomePage = () => {
+  const user = window.Telegram.WebApp.initDataUnsafe.user;
+
   return (
     <Wrapper>
+      {JSON.stringify(user)}
       <Menu />
     </Wrapper>
   );
