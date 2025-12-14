f1ff505a844da2cec878431eb849c3191adf6f0a
Jahongir <nusratovjahongir@gmail.com>
2025-12-14 21:07:19 +0500

    refactor: add author to card

--- src/pages/my-tests/ui/list/item.tsx
+++ src/pages/my-tests/ui/list/item.tsx
@@ -58,6 +58,18 @@ const MyTestItem: React.FC<Props> = ({ test }) => {
             <InfoItem label="Total tests" value={test.totalTests} />
             <InfoItem label="Semestr" value={test.semestr} />
           </ul>
+
+          <div className="bg-secondary mt-2 flex items-center gap-2 rounded-lg p-2">
+            <div className="h-10 w-10 overflow-hidden rounded-full border">
+              <img src="https://picsum.photos/40/40" alt="" />
+            </div>
+            <div>
+              <h4 className="text-sm font-semibold text-slate-800">
+                {test.author}
+              </h4>
+              <h4 className="text-xs text-blue-500">@Mr_Jakhongir</h4>
+            </div>
+          </div>
         </CardContent>
       </Card>
     </li>
